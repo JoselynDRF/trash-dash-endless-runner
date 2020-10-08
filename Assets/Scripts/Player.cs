90ec0b4665bf6835f8b2b5e23687a989d2f6a544
JoselynDRF <mariajoselyndosramos@yahoo.es>
2020-10-08 15:55:30 +0100

    Add player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    public float runSpeed = 10f;
+    public float laneChangeSpeed = 10f;
+
     private Animator animator;
     private Rigidbody rb;
-    private int runSpeed = 10;
+    private Vector3 targetPosition;
+    private int currentLane = 0;
 
     void Start() {
         rb = GetComponent<Rigidbody>();
@@ -15,6 +17,27 @@ public class Player : MonoBehaviour {
     }
 
     void Update() {
-      rb.velocity = Vector3.forward * runSpeed;
+      MoveCharacter();
+    }
+
+    void MoveCharacter() {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) ChangeLane(-1);
+        else if (Input.GetKeyDown(KeyCode.RightArrow)) ChangeLane(1);
+
+        Vector3 newPosition = new Vector3(targetPosition.x, transform.position.y, transform.position.z);
+        transform.localPosition = Vector3.MoveTowards(transform.position, newPosition, laneChangeSpeed * Time.deltaTime);
+    }
+
+    void ChangeLane(int direction) {
+        int targetLane = currentLane + direction;
+
+        if (targetLane >= -1 && targetLane <= 1) {
+            targetPosition = new Vector3(targetLane, 0, 0);
+            currentLane = targetLane; 
+        }
+    }
+
+    void FixedUpdate() {
+        rb.velocity = Vector3.forward * runSpeed;
     }
 }
