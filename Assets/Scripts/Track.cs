cc4fdc2dc3512a2daa3da9510a5351fba5342463
JoselynDRF <mariajoselyndosramos@yahoo.es>
2020-10-09 21:16:03 +0100

    Add tracks

---
+++ Assets/Scripts/Track.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Track : MonoBehaviour {
+
+    private float endPosition = 314f;
+
+    void Start() {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            transform.position = new Vector3(0, 0, transform.position.z + endPosition * 2);
+        }
+    }
+
+}
