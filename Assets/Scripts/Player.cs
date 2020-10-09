18563de8674eb23eccf293bfe1fbfec82f928b4c
JoselynDRF <mariajoselyndosramos@yahoo.es>
2020-10-09 16:54:59 +0100

    Add touch input for mobile

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,20 +4,22 @@ public class Player : MonoBehaviour {
 
     public float runSpeed = 10f;
     public float laneChangeSpeed = 10f;
-    public float jumpLength = 7.5f;
-    public float jumpHeight = 1f;
     public float jumpSpeed = 5f;
+    public float jumpLength = 7.5f;
     public float slideLength = 10f;
+    public float jumpHeight = 1f;
 
     private Animator animator;
     private Rigidbody rb;
     private BoxCollider boxCollider;
     private Vector3 targetPosition;
     private Vector3 boxColliderSize;
-    private float jumpStart;
+    private Vector2 startingTouch;
+    private bool isSwiping = false;
     private bool isJumping = false;
-    private float slideStart;
     private bool isSliding = false;
+    private float jumpStart;
+    private float slideStart;
 
     void Start() {
         rb = GetComponent<Rigidbody>();
@@ -37,6 +39,7 @@ public class Player : MonoBehaviour {
 
     void MoveCharacter() {
         HandleKeyboard();
+        HandleTouch();
         HandleJump();
         HandleSlide();
 
@@ -49,6 +52,48 @@ public class Player : MonoBehaviour {
         else if (Input.GetKeyDown(KeyCode.RightArrow)) ChangeLane(1);
         else if (Input.GetKeyDown(KeyCode.UpArrow)) Jump();
         else if (Input.GetKeyDown(KeyCode.DownArrow)) Slide(); 
+    }
+
+    void HandleTouch() {
+        if (Input.touchCount == 1) {
+            if (isSwiping) {
+                Vector2 diff = Input.GetTouch(0).position - startingTouch;
+                diff = new Vector2(diff.x / Screen.width, diff.y / Screen.width);
+                
+                if (diff.magnitude > 0.01f) {
+                    if (Mathf.Abs(diff.y) > Mathf.Abs(diff.x)) {
+                        HandleVerticalSwiping(diff.y);
+                    } else {
+                        HandleHorizontalSwipping(diff.x);
+                    }
+
+                    isSwiping = false;
+                }
+            }
+      
+            validateSwiping();
+        }
+    }
+
+    void HandleVerticalSwiping(float diffY) {
+        if (diffY < 0) Slide();
+        else Jump();
+    }
+
+    void HandleHorizontalSwipping(float diffX) {
+        if (diffX < 0) ChangeLane(-1);
+        else ChangeLane(1);
+    }
+
+    void validateSwiping() {
+        if (Input.GetTouch(0).phase == TouchPhase.Began) {
+            startingTouch = Input.GetTouch(0).position;
+            isSwiping = true;
+        }
+
+        if (Input.GetTouch(0).phase == TouchPhase.Ended) {
+            isSwiping = false;
+        }
     }
 
     void ChangeLane(int direction) {
