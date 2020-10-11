0ec225045b785cb7d85bc14ea68aa1e476c04d3a
JoselynDRF <mariajoselyndosramos@yahoo.es>
2020-10-11 17:06:02 +0100

    Fix bug in obstacles creation

--- Assets/Scripts/Track.cs
+++ Assets/Scripts/Track.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class Track : MonoBehaviour {
 
-    public GameObject coin;
+    public GameObject[] coins;
     public GameObject[] obstacles;
     public Vector2 coinsNumber;
     public Vector2 obstaclesNumber;
@@ -11,24 +11,16 @@ public class Track : MonoBehaviour {
     private float trackEndPosition = 314f;
 
     void Start() {
-        CreateCoins();
-        CreateObstacles();
+        CreateTrackItems(coinsNumber, coins);
+        CreateTrackItems(obstaclesNumber, obstacles);
     }
 
-    void CreateCoins() {
-        CreateTrackItems(coinsNumber, coin);
-    }
-
-    void CreateObstacles() {
-        GameObject obstacle = obstacles[Random.Range(0, obstacles.Length)];
-        CreateTrackItems(obstaclesNumber, obstacle);
-    }
-
-    void CreateTrackItems(Vector2 itemsNumber, GameObject item) {
+    void CreateTrackItems(Vector2 itemsNumber, GameObject[] items) {
         List<GameObject> itemsList = new List<GameObject>();
         int newItemsNumber = (int) Random.Range(itemsNumber.x, itemsNumber.y);
 
         for (int i = 0; i < newItemsNumber; i++) {
+            GameObject item = items[Random.Range(0, items.Length)];
             float itemPosition = (trackEndPosition / newItemsNumber) + (trackEndPosition / newItemsNumber) * i;
             float randomPosition = Random.Range(itemPosition, itemPosition + 1);
 
